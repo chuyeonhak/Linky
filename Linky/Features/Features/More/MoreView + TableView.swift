2efc70f2b6134e8d03dbafdfa338655365cb15e8
Swift_Chu <chuyeonhak@nate.com>
2023-06-02 08:58:24 +0900

    Feat: Add moreView

--- Linky/Features/Features/More/MoreView + TableView.swift
+++ Linky/Features/Features/More/MoreView + TableView.swift
@@ -5,4 +5,29 @@
 //  Created by chuchu on 2023/06/01.
 //
 
-import Foundation
+import UIKit
+
+extension MoreView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        SettingType.allCases.count
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: MoreCell.identifier,
+            for: indexPath) as? MoreCell,
+              let type = SettingType.allCases[safe: indexPath.row]
+            else { return UITableViewCell() }
+        
+        cell.configure(type: type)
+        return cell
+    }
+}
+
+extension MoreView: UITableViewDelegate {
+    
+}
+
+
--- Linky/Features/Features/More/MoreView.swift
+++ Linky/Features/Features/More/MoreView.swift
@@ -5,4 +5,55 @@
 //  Created by chuchu on 2023/06/01.
 //
 
-import Foundation
+import UIKit
+
+import Core
+
+import SnapKit
+import Then
+import RxSwift
+
+final class MoreView: UIView {
+    lazy var tableView = UITableView().then {
+        $0.addCornerRadius(radius: 12)
+        $0.separatorStyle = .none
+        $0.allowsSelection = false
+//        $0.isScrollEnabled = false
+        $0.delegate = self
+        $0.dataSource = self
+        $0.register(MoreCell.self, forCellReuseIdentifier: MoreCell.identifier)
+        $0.addShadow(offset: CGSize(width: 0, height: 0), opacity: 0.16, blur: 10)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        addComponent()
+        setConstraints()
+        bind()
+    }
+    
+    private func addComponent() {
+        addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        backgroundColor = .code7
+        
+        tableView.backgroundColor = .code8
+        tableView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(288)
+        }
+    }
+    
+    private func bind() { }
+}
+
