858069355900d53701abfd248ebe3474b7c370bd
Swift_Chu <chuyeonhak@nate.com>
2023-06-02 20:01:01 +0900

    Feat: Add LineTextField

--- Linky/Features/Features/Design/LineTextField.swift
+++ Linky/Features/Features/Design/LineTextField.swift
@@ -5,4 +5,52 @@
 //  Created by chuchu on 2023/06/02.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+import RxSwift
+
+final class LineTextField: UITextField {
+    private let lineView = UIView().then {
+        $0.backgroundColor = .code1
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        setTextField()
+        addComponent()
+        setConstraints()
+        bind()
+    }
+    
+    private func setTextField() {
+        textColor = .code2
+        tintColor = .code4
+        
+//        textAlignment = .natural
+        clearButtonMode = .whileEditing
+    }
+    
+    private func addComponent() {
+        addSubview(lineView)
+    }
+    
+    private func setConstraints() {
+        lineView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+    }
+    
+    private func bind() { }
+}
+
