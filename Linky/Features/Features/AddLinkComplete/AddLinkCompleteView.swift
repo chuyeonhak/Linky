2fae6b1ec13a64e351789adc9784a236d8d444f0
Swift_Chu <chuyeonhak@nate.com>
2023-06-05 18:57:57 +0900

    Feat: Add LinkCompleteView

--- Linky/Features/Features/AddLinkComplete/AddLinkCompleteView.swift
+++ Linky/Features/Features/AddLinkComplete/AddLinkCompleteView.swift
@@ -1,8 +1,38 @@
 //
-//  File.swift
+//  AddLinkCompleteView.swift
 //  Features
 //
 //  Created by chuchu on 2023/06/05.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+import RxSwift
+
+final class AddLinkCompleteView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func commonInit() {
+        addComponent()
+        setConstraints()
+        bind()
+    }
+    
+    private func addComponent() {
+        self.backgroundColor = .code8
+    }
+    
+    private func setConstraints() { }
+    
+    private func bind() { }
+}
+
--- Linky/Features/Features/AddLinkComplete/AddLinkCompleteViewController.swift
+++ Linky/Features/Features/AddLinkComplete/AddLinkCompleteViewController.swift
@@ -5,4 +5,53 @@
 //  Created by chuchu on 2023/06/05.
 //
 
-import Foundation
+import UIKit
+
+import Core
+
+import SnapKit
+import Then
+import RxSwift
+
+final class AddLinkCompleteViewContrller: UIViewController {
+    var addLinkCompleteView: AddLinkCompleteView!
+    let disposeBag = DisposeBag()
+    
+    override func loadView() {
+        let addLinkCompleteView = AddLinkCompleteView()
+        
+        self.addLinkCompleteView = addLinkCompleteView
+        self.view = addLinkCompleteView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationSetting()
+    }
+    
+    private func navigationSetting() {
+        let rightItem = makeRightItem()
+        
+        navigationItem.hidesBackButton = true
+        navigationItem.rightBarButtonItem = rightItem
+        navigationController?.navigationBar.topItem?.title = ""
+        
+    }
+    
+    private func makeRightItem() -> UIBarButtonItem {
+        let rightButton = UIButton()
+        
+        rightButton.setImage(UIImage(named: "icoXmark"), for: .normal)
+        
+        rightButton.rx.tap
+            .bind { [weak self] in
+                self?.navigationController?.popToRootViewController(animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        return UIBarButtonItem(customView: rightButton)
+    }
+}
+
+
