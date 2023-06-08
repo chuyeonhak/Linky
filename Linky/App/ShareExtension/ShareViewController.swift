93d1176b01221ac8ae154db9e58b2f5abe08df9d
Swift_Chu <chuyeonhak@nate.com>
2023-06-08 14:24:14 +0900

    Feat: Add ShareViewController

--- Linky/App/ShareExtension/ShareViewController.swift
+++ Linky/App/ShareExtension/ShareViewController.swift
@@ -9,23 +9,23 @@
 import UIKit
 import Social
 
-class ShareViewController: SLComposeServiceViewController {
-
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
-    }
-
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+class ShareViewController: UIViewController {
+//    let someView = AddLinkView()
     
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    override func loadView() {
+        super.loadView()
+        
+//        let some = TestFeature()
+        let someView = UIView()
+        someView.backgroundColor = .red
+    
+        
+        self.view = someView
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
     }
-
 }
