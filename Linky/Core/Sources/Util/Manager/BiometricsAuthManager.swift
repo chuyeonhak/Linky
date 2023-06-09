5a8f6a4e8d5ac2112b388cd9c75bd2cbcf965aa6
Swift_Chu <chuyeonhak@nate.com>
2023-06-09 18:13:21 +0900

    Feat: Add BiometricsAuthManager

--- Linky/Core/Sources/Util/Manager/BiometricsAuthManager.swift
+++ Linky/Core/Sources/Util/Manager/BiometricsAuthManager.swift
@@ -5,4 +5,21 @@
 //  Created by chuchu on 2023/06/08.
 //
 
-import Foundation
+import LocalAuthentication
+
+public class BiometricsAuthManager {
+    public static func biometricType() -> LABiometryType {
+        let authContext = LAContext()
+        let _ = authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+        switch(authContext.biometryType) {
+        case .none:
+            return .none
+        case .touchID:
+            return .touchID
+        case .faceID:
+            return .faceID
+        @unknown default:
+            fatalError()
+        }
+    }
+}
