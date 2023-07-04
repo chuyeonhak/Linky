d526383a8c5ef4f771defb87898009bc6860fe10
Swift_Chu <chuyeonhak@nate.com>
2023-07-04 10:16:21 +0900

    Feat: Add OpenGraphManager

--- Linky/Core/Sources/Util/Manager/OpenGraphManager.swift
+++ Linky/Core/Sources/Util/Manager/OpenGraphManager.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+import Alamofire
+import OpenGraph
+
+public struct OpenGraphManager {
+    public static let shared = OpenGraphManager()
+    
+    public func getMetaData(urlString: String,
+                            completion: ((MetaData) -> ())? = nil) {
+        getHtml(urlString) { html in
+            if let html,
+               case let openGraph = OpenGraph(htmlString: html) {
+                completion?(MetaData(openGraph: openGraph, url: urlString))
+            } else {
+                completion?(MetaData(url: urlString))
+            }
+        }
+    }
+    
+    private func getHtml(_ urlString: String, completion: ((String?) -> ())? = nil) {
+        AF.request(urlString).responseString { response in
+            if let html = response.value {
+                completion?(html)
+            } else {
+                completion?(nil)
+            }
+        }
+    }
+}
