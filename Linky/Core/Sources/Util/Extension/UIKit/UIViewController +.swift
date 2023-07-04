8364528367cb7cef8a5e405b9adb6bf7dfaf2331
Swift_Chu <chuyeonhak@nate.com>
2023-07-04 10:15:36 +0900

    Feat: Extension UIViewController

--- Linky/Core/Sources/Util/Extension/UIKit/UIViewController +.swift
+++ Linky/Core/Sources/Util/Extension/UIKit/UIViewController +.swift
@@ -5,4 +5,26 @@
 //  Created by chuchu on 2023/07/03.
 //
 
-import Foundation
+import UIKit
+
+public extension UIViewController {
+    func presentAlertController(style: UIAlertController.Style = .alert,
+                                title: String? = nil,
+                                message: String? = nil,
+                                options: (title: String, style: UIAlertAction.Style)...,
+                                completion: ((String) -> Void)?) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: style)
+        
+        for (index, option) in options.enumerated() {
+            alertController.addAction(UIAlertAction(title: option.title,
+                                                    style: option.style,
+                                                    handler: { _ in
+                completion?(options[index].title)
+            }))
+        }
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
