7a7f6b3ca660d0456195da9655e132f8ed8560ae
Swift_Chu <chuyeonhak@nate.com>
2023-07-04 10:15:57 +0900

    Feat: Add HapticManager

--- Linky/Core/Sources/Util/Manager/HapticManager.swift
+++ Linky/Core/Sources/Util/Manager/HapticManager.swift
@@ -5,4 +5,35 @@
 //  Created by chuchu on 2023/06/28.
 //
 
-import Foundation
+import UIKit
+
+import Then
+
+public struct HapticManager {
+    public static let shared = HapticManager()
+    
+    private let notification = UINotificationFeedbackGenerator().then {
+        $0.prepare()
+    }
+    
+    private var impact: UIImpactFeedbackGenerator?
+    
+    private let select = UISelectionFeedbackGenerator().then {
+        $0.prepare()
+    }
+    
+    public func notification(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+        notification.notificationOccurred(type)
+    }
+    
+    public mutating func impact(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        self.impact = UIImpactFeedbackGenerator(style: style)
+        impact?.prepare()
+        
+        impact?.impactOccurred()
+    }
+    
+    public func selection() {
+        select.selectionChanged()
+    }
+}
