cf7aaa9e5c9462bcd5607797bee032c7abd1da53
Swift_Chu <chuyeonhak@nate.com>
2023-07-04 10:16:08 +0900

    Feat: Add IndicatorManager

--- Linky/Core/Sources/Util/Manager/IndicatorManager.swift
+++ Linky/Core/Sources/Util/Manager/IndicatorManager.swift
@@ -5,4 +5,34 @@
 //  Created by chuchu on 2023/07/03.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+public final class IndicatorManager {
+    public static let shared = IndicatorManager()
+    private var indicatorView: UIActivityIndicatorView?
+    
+    private func setIndicatorView() {
+        let indicatorView = UIActivityIndicatorView()
+        
+        indicatorView.style = .large
+        
+        self.indicatorView = indicatorView
+        UIApplication.shared.window?.rootViewController?.view.addSubview(indicatorView)
+        
+        indicatorView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    public func startAnimation() {
+        setIndicatorView()
+        indicatorView?.startAnimating()
+    }
+    
+    public func stopAnimation() {
+        indicatorView?.stopAnimating()
+        indicatorView?.removeFromSuperview()
+    }
+}
