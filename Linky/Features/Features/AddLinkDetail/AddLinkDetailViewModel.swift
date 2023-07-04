712fa2adc6d63f533bac907d76aa468870e64b85
Swift_Chu <chuyeonhak@nate.com>
2023-07-04 10:20:20 +0900

    Feat: Add LinkDetailViewModel

--- Linky/Features/Features/AddLinkDetail/AddLinkDetailViewModel.swift
+++ Linky/Features/Features/AddLinkDetail/AddLinkDetailViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+import Core
+
+import RxSwift
+import RxRelay
+import RxCocoa
+
+final class AddLinkDetailViewModel {
+    struct Input {
+        let deleteIndexPath = PublishSubject<IndexPath>()
+    }
+    
+    struct Model { }
+    
+    struct Output {
+        let deleteIndexPath: Driver<IndexPath>
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    let model = Model()
+    let input = Input()
+    var output: Output?
+    
+    init() {
+        self.output = Output(
+            deleteIndexPath: input.deleteIndexPath.asDriverOnErrorEmpty()
+        )
+    }
+}
+
