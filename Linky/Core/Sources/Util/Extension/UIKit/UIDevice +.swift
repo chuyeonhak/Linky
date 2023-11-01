b7437ad0ad26379253c6eadbb006af3b97ab3260
Swift_Chu <chuyeonhak@nate.com>
2023-11-01 16:26:16 +0900

    Feat: Add UIDevice Extension - isIpad

--- Linky/Core/Sources/Util/Extension/UIKit/UIDevice +.swift
+++ Linky/Core/Sources/Util/Extension/UIKit/UIDevice +.swift
@@ -1,8 +1,14 @@
 //
-//  UIDevice.swift
+//  UIDevice +.swift
 //  Core
 //
 //  Created by chuchu on 2023/07/13.
 //
 
-import Foundation
+import UIKit
+
+public extension UIDevice {
+    var isIpad: Bool {
+        return UIDevice.current.userInterfaceIdiom == .pad
+    }
+}
