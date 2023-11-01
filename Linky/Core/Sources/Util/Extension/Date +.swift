93db71e5632d680d58c8e0a3b5794f43eb87d2d9
Swift_Chu <chuyeonhak@nate.com>
2023-11-01 16:25:07 +0900

    Feat: Add dateToString Function

--- Linky/Core/Sources/Util/Extension/Date +.swift
+++ Linky/Core/Sources/Util/Extension/Date +.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+public extension Date {
+    func dateToString() -> String {
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy.MM.dd HH:mm"
+        formatter.locale = Locale(identifier: "ko_KR")
+        
+        return formatter.string(from: self)
+    }
+}
