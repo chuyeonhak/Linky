8e75ad24c5f4c4fb04c7b17d84206bd9b793178c
Swift_Chu <chuyeonhak@nate.com>
2023-11-01 16:24:13 +0900

    Feat: Add NotificationInfo Model

--- Linky/Core/Sources/Model/Notification.swift
+++ Linky/Core/Sources/Model/Notification.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+
+public struct NotificationInfo: Codable {
+    public let days: Days
+    public var selected: Bool
+}
+
+extension NotificationInfo: Equatable { }
+
+public struct NotificationSetting: Codable {
+    public var info: [NotificationInfo]
+    public var time: Date?
+}
+
+extension NotificationSetting: Equatable { }
+
+public enum Days: Int {
+    case sun = 1
+    case mon
+    case tue
+    case wed
+    case thu
+    case fri
+    case sat
+    
+    public var korean: String {
+        switch self {
+        case .sun:
+            return "일요일"
+        case .mon:
+            return "월요일"
+        case .tue:
+            return "화요일"
+        case .wed:
+            return "수요일"
+        case .thu:
+            return "목요일"
+        case .fri:
+            return "금요일"
+        case .sat:
+            return "토요일"
+        }
+    }
+}
+
+extension Days: CaseIterable, Codable { }
