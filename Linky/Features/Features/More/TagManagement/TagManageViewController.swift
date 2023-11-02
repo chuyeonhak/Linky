c6e595e95197836931ec3900cf8d8cf3cca6d912
Swift_Chu <chuyeonhak@nate.com>
2023-11-02 13:27:50 +0900

    Feat: Add deleteTag function in TagManageView

--- Linky/Features/Features/More/TagManagement/TagManageViewController.swift
+++ Linky/Features/Features/More/TagManagement/TagManageViewController.swift
@@ -13,20 +13,20 @@ import SnapKit
 import Then
 import RxSwift
 
-final class TagManageViewController: UIViewController {
+public final class TagManageViewController: UIViewController {
     var tagManageView: TagManageView!
     let disposeBag = DisposeBag()
     let viewModel = TagManageViewModel()
     var tagList = UserDefaultsManager.shared.tagList
     
-    override func loadView() {
+    public override func loadView() {
         let tagManageView = TagManageView(viewModel: viewModel)
         
         self.tagManageView = tagManageView
         self.view = tagManageView
     }
     
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationButton()
@@ -77,22 +77,44 @@ final class TagManageViewController: UIViewController {
     }
     
     private func tagHandle(_ handle: TagManageViewModel.TagHandle) {
-        var tagList = UserDefaultsManager.shared.tagList
         switch handle.type {
         case .delete:
-            tagManageView.selectedItems.remove(at: handle.row)
-            tagList.remove(at: handle.row)
-            UserDefaultsManager.shared.tagList = tagList
-            
-            tagManageView.tagTableView.deleteRows(at: [IndexPath(item: handle.row, section: 0)],
-                                                  with: .automatic)
+            openAlert(handle: handle)
             
         case .edit where tagList.indices ~= handle.row:
             openTagController(type: .edit, tagData: tagList[handle.row])
         default: break
         }
+    }
+    
+    private func openAlert(handle: TagManageViewModel.TagHandle) {
+        let tag = UserDefaultsManager.shared.tagList[safe: handle.row]
+        let title = "\"\(tag?.title ?? "")\" 태그를 삭제할까요?"
+        let message = "연결된 모든 링크에서 태그가 삭제됩니다."
+        presentAlertController(
+            title: title,
+            message: message,
+            options: (title: "취소", style: .default), (title: "삭제", style: .destructive)) {
+                if $0 == "삭제" {
+                    self.deleteTag(handle: handle)
+                }
+            }
+    }
+    
+    private func deleteTag(handle: TagManageViewModel.TagHandle) {
+        var tagList = UserDefaultsManager.shared.tagList
+        let tag = tagList[safe: handle.row]
+        tagManageView.selectedItems.remove(at: handle.row)
+        tagList.remove(at: handle.row)
+        UserDefaultsManager.shared.tagList = tagList
+
+        tagManageView.tagTableView.deleteRows(at: [IndexPath(item: handle.row, section: 0)],
+                                              with: .automatic)
+
+        UserDefaultsManager.shared.deleteTagInLink(tag: tag)
         
-        checkTagList()
+        tagManageView.titleLabel.text = "\(tagList.count)개의 태그가 있어요."
+        setNavigationItem(selectedCount: 0)
     }
     
     private func setNavigationItem(selectedCount: Int) {
@@ -133,11 +155,27 @@ final class TagManageViewController: UIViewController {
             
             tagHandle((.edit, firstIndex))
         case .delete where selectedItemCount > 0:
-            tagManageView.selectedItems.enumerated().reversed().forEach { index, isSelected in
-                if isSelected {
-                    tagHandle((.delete, index))
+            let tagList = UserDefaultsManager.shared.tagList,
+                selectedItems = tagManageView.selectedItems
+            let titleList = zip(tagList, selectedItems)
+                .filter ({ $0.1 })
+                .map { "\"\($0.0.title)\"" }
+                .joined(separator: ", ")
+            
+            let title = selectedItems.filter { $0 }.count == 1 ? titleList: "[\(titleList)]"
+            
+            presentAlertController(
+                title: "\(title) 태그를 삭제할까요?",
+                message: "연결된 모든 링크에서 태그가 삭제됩니다.",
+                options: (title: "취소", style: .default), (title: "삭제", style: .destructive)) {
+                    if $0 == "삭제" {
+                        selectedItems.enumerated().reversed().forEach { index, isSelected in
+                            if isSelected {
+                                self.deleteTag(handle: (type: .delete, row: index))
+                            }
+                        }
+                    }
                 }
-            }
             
         default: break
         }
