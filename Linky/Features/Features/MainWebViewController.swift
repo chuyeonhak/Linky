7c68db8e3239cca4e28ae49f13b690651fbec0ae
Swift_Chu <chuyeonhak@nate.com>
2023-11-02 09:59:11 +0900

    Feat: Add MainWebViewController

--- Linky/Features/Features/MainWebViewController.swift
+++ Linky/Features/Features/MainWebViewController.swift
@@ -1,8 +1,59 @@
 //
-//  WebViewController.swift
+//  MainWebViewController.swift
 //  Features
 //
 //  Created by chuchu on 2023/07/14.
 //
 
-import Foundation
+import UIKit
+import WebKit
+
+import Core
+
+import SnapKit
+import Then
+import RxSwift
+
+final class MainWebViewController: UIViewController {
+    let linkUrl: URL!
+    
+    let webView = WKWebView()
+    
+    init(linkUrl: URL) {
+        self.linkUrl = linkUrl
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        self.view = webView
+        let request = URLRequest(url: linkUrl)
+        webView.load(request)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.barTintColor = .code7
+        navigationController?.navigationBar.backgroundColor = .code7
+        
+        UIApplication.shared.windows.first?.viewWithTag(Tag.statusBar)?.backgroundColor = .code7
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let tabBar = tabBarController as? RootViewController
+        
+        tabBar?.tabBarAnimation(shouldShow: false)
+    }
+    
+    deinit { print(description, "deinit") }
+}
