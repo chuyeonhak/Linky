3ec078ca488983660a2ce406ca1c1731742fe3b4
Swift_Chu <chuyeonhak@nate.com>
2024-02-16 10:18:18 +0900

    Feat: UILabel extension textColorChange, textFontChange

--- Linky/Core/Sources/Util/Extension/UIKit/UILabel +.swift
+++ Linky/Core/Sources/Util/Extension/UIKit/UILabel +.swift
@@ -5,4 +5,22 @@
 //  Created by chuchu on 11/13/23.
 //
 
-import Foundation
+import UIKit
+
+public extension UILabel {
+    func textColorChange(text: String, color: UIColor?, range: String){
+        guard let color else { return }
+        
+        let attributedStr = NSMutableAttributedString(string: text)
+        attributedStr.addAttribute(.foregroundColor, value: color, range: (text as NSString).range(of: range))
+        
+        self.attributedText = attributedStr
+    }
+    
+    func textFontChange(text: String, font: UIFont, range: String){
+        let attributedStr = NSMutableAttributedString(string: text)
+        attributedStr.addAttribute(.font, value: font, range: (text as NSString).range(of: range))
+        
+        self.attributedText = attributedStr
+    }
+}
