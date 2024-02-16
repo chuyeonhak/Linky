0214fd0e3a54ec884e98e51e97323d9f4e7c8d0a
Swift_Chu <chuyeonhak@nate.com>
2024-02-16 10:20:29 +0900

    Feat: UITextView add property placeholder

--- Linky/Core/Sources/Util/Extension/UIKit/UITextView +.swift
+++ Linky/Core/Sources/Util/Extension/UIKit/UITextView +.swift
@@ -5,4 +5,14 @@
 //  Created by chuchu on 11/13/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+public extension UITextView {
+    var placeholder: UILabel? {
+        subviews
+            .filter { $0.tag == 100 }
+            .compactMap { $0 as? UILabel }.first
+    }
+}
