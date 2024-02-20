3daf18dde77f6b11ea767365c8c44dff8ebb1983
Swift_Chu <chuyeonhak@nate.com>
2024-02-20 16:23:50 +0900

    Feat: String Add property localized

--- Linky/Core/Sources/Util/Extension/String +.swift
+++ Linky/Core/Sources/Util/Extension/String +.swift
@@ -1,8 +1,12 @@
 //
-//  String.swift
+//  String +.swift
 //  Core
 //
 //  Created by chuchu on 12/27/23.
 //
 
 import Foundation
+
+public extension String {
+    var localized: String { NSLocalizedString(self, comment: "") }
+}
