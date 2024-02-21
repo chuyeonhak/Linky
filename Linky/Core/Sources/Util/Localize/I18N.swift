fd586cf4cb91e420e8f1bd5b06ca8846d2e9bbb6
Swift_Chu <chuyeonhak@nate.com>
2024-02-21 13:58:40 +0900

    Fix: I18N add text

--- Linky/Core/Sources/Util/Localize/I18N.swift
+++ Linky/Core/Sources/Util/Localize/I18N.swift
@@ -1,42 +1,140 @@
-public struct I18N {
+public enum I18N {
+    /// 링크 추가하기
     public static let addLink = "addLink".localized
+    /// 추가할 링크를 입력해주세요.
     public static let addLinkPlaceholder = "addLinkPlaceholder".localized
+    /// 링크 추가 완료!
     public static let addLinkToast = "addLinkToast".localized
+    /// 링키 바로 추가하기
     public static let addLinkyText = "addLinkyText".localized
+    /// 태그 추가
     public static let addTag = "addTag".localized
+    /// 추가할 태그를 입력해 주세요.
+    public static let addTagPlaceholder = "addTagPlaceholder".localized
+    /// 전체
     public static let all = "all".localized
+    /// 취소
     public static let cancel = "cancel".localized
+    /// 카테고리
+    public static let category = "category".localized
+    /// 오류가 발생했어요.
+    public static let categoryError = "categoryError".localized
+    /// 기타 문의
+    public static let categoryEtc = "categoryEtc".localized
+    /// 문의 유형을 선택해 주세요.
+    public static let categoryNone = "categoryNone".localized
+    /// 이런 게 필요해요.
+    public static let categoryWant = "categoryWant".localized
+    /// 비밀번호 변경
+    public static let changePassword = "changePassword".localized
+    /// 비우기
+    public static let clear = "clear".localized
+    /// 완료
     public static let completed = "completed".localized
+    /// 요일 설정
     public static let daySetting = "daySetting".localized
+    /// 새로운 암호 입력
+    public static let enterNewPassword = "enterNewPassword".localized
+    /// 새로운 암호를 입력해 주세요.
+    public static let enterNewPasswordSubtitle = "enterNewPasswordSubtitle".localized
+    /// 비밀번호 입력
     public static let enterPassword = "enterPassword".localized
+    /// 확인을 위해 한 번 더 입력해 주세요.
+    public static let enterPasswordAgain = "enterPasswordAgain".localized
+    /// 비밀번호 네 자리를 입력해 주세요.
     public static let enterPasswordSubtitle = "enterPasswordSubtitle".localized
+    /// 검색어를 입력해 주세요.
     public static let findTagPlaceholder = "findTagPlaceholder".localized
+    /// 금요일
     public static let friday = "friday".localized
+    /// 문의하기
     public static let inquiry = "inquiry".localized
+    /// 문의내용
+    public static let inquiryContent = "inquiryContent".localized
+    /// 마음이 여린 개발자입니다.비속어는 개발자의 마음을 아프게 합니다.
+    public static let inquiryPlaceholder = "inquiryPlaceholder".localized
+    /// #COUNT#개의 링크가 있어요.
+    public static let linkCountText = "linkCountText".localized
+    /// 링크 휴지통
     public static let linkManagement = "linkManagement".localized
+    /// 링크 메모
     public static let linkMemo = "linkMemo".localized
+    /// 메모 입력(띄어쓰기 포함, 30자 이내)
     public static let linkPlaceholder = "linkPlaceholder".localized
+    /// 화면 잠금 설정
     public static let lock = "lock".localized
+    /// 월요일
     public static let monday = "monday".localized
+    /// 더보기
     public static let more = "more".localized
+    /// 태그 없음
     public static let noTags = "noTags".localized
+    /// 링키를 보고 싶은 요일과 시간을 정해보세요 :)
     public static let notificationGuideText = "notificationGuideText".localized
+    /// 알림설정
     public static let notificationSettings = "notificationSettings".localized
+    /// 전에 입력한 번호와 일치하지 않아요.
+    public static let passwordErrorSubtitle = "passwordErrorSubtitle".localized
+    /// 비밀번호를 잃어버릴 경우
+    public static let passwordGuide = "passwordGuide".localized
+    /// 복사한 링크 붙여넣기
     public static let pasteButtonText = "pasteButtonText".localized
+    /// 이전 30일
+    public static let previous30Day = "previous30Day".localized
+    /// 이전 7일
+    public static let previous7Day = "previous7Day".localized
+    /// 읽음
     public static let read = "read".localized
+    /// 복원하기
+    public static let recover = "recover".localized
+    /// 토요일
     public static let saturday = "saturday".localized
+    /// 저장하기
     public static let save = "save".localized
+    /// 전체선택
+    public static let selectAll = "selectAll".localized
+    /// 제출하기
+    public static let submit = "submit".localized
+    /// 문의를 제출할까요?
+    public static let submitAlertTitle = "submitAlertTitle".localized
+    /// 문의 완료!
+    public static let submitCompleted = "submitCompleted".localized
+    /// 일요일
     public static let sunday = "sunday".localized
+    /// 태그
     public static let tag = "tag".localized
+    /// #COUNT#개의 태그가 있어요.
+    public static let tagCountText = "tagCountText".localized
+    /// 연결된 링크가 있는 태그를 삭제하면해당 링크는 '태그 없음'에 연결돼요.
+    public static let tagManageSubtitle = "tagManageSubtitle".localized
+    /// 태그 관리
     public static let tagManagement = "tagManagement".localized
+    /// 태그 입력(띄어쓰기 포함, 8자 이내)
     public static let tagPlaceholder = "tagPlaceholder".localized
+    /// 목요일
     public static let thursday = "thursday".localized
+    /// 내가 추가한 링크를시간 순서대로 볼 수 있어요.
     public static let timeEmptyText = "timeEmptyText".localized
+    /// 시간 설정
     public static let timeSetting = "timeSetting".localized
+    /// 타임라인
     public static let timeline = "timeline".localized
+    /// 링키 120% 활용하기
     public static let tips = "tips".localized
+    /// 오늘
+    public static let today = "today".localized
+    /// 화요일
     public static let tuesday = "tuesday".localized
+    /// 안 읽음
     public static let unread = "unread".localized
+    /// 생체인증 사용
+    public static let useBiometrics = "useBiometrics".localized
+    /// 알림 사용
     public static let useNotifications = "useNotifications".localized
+    /// 수요일
     public static let wednesday = "wednesday".localized
+    /// 년
+    public static let year = "year".localized
+    /// 어제
+    public static let yesterday = "yesterday".localized
 }
